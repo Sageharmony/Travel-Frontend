3fbc931250eb9328ac4d6eb2cc2768c74b3778c3
Sage Kolpin <sagekolpin@Sages-Air.fios-router.home>
2022-05-28 09:21:56 -0700

    working fontend maybe?

--- src/App.js
+++ src/App.js
@@ -1,25 +1,122 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios'
+const App = () =>{
+const [destination, setDestination] = useState([])
+const [location, setLocation] = useState('')
+const [sights, setSights] = useState([])
+const [img, setImg] = useState('')
+const [restaurants, setRestaurants] = useState([])
+const [time, setTime] = useState('')
+const [cost, setCost] = useState(2)
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const handleLocation = (event) =>{
+  setLocation(event.target.value)
+}
+const handleSights = (event) =>{
+  setSights(event.target.value)
+}
+const handleImg = (event) =>{
+  setImg(event.target.value)
+}
+const handleRestaurants = (event) =>{
+  setRestaurants(event.target.value)
+}
+const handleTime = (event) =>{
+  setTime(event.target.value)
+}
+const handleCost = (event) =>{
+  setCost(event.target.value)
+}
+const addLocation = (event) =>{
+  event.preventDefault()
+  axios.post(
+    '/', {
+      location: location,
+      mustSee: sights,
+      image: img,
+      restaurantsToTry: restaurants,
+      bestTime: time,
+      costPerPerson: cost  
+    }
+  ).then(() =>{
+    axios.get('/').then( (response) =>{
+      ServiceWorkerContainer(response.data)
+    })
+  })
+}
+const handleLocationUpdate = (event, locationData) =>{
+  event.preventDefault();
+  axios.put(`/${locationData._id}`,
+    {
+    location: location,
+    mustSee: sights,
+    image: img,
+    restaurantsToTry: restaurants,
+    bestTime: time,
+    costPerPerson: cost
+  }).then(() =>{
+    axios.get('/').then((response) =>{
+      setDestination(response.data)
+    })
+  })
+}
+useEffect(()=>{
+  axios
+      .get('')
+      .then((response)=>{
+        setDestination(response.data);
+      })
+},[])
+
+
+const handleLocationDelete = (event, locationData) =>{
+  axios.delete(`/${locationData._id}`,).then(() =>{
+axios.get('/').then((response) =>{
+setDestination(response.data)
+})
+  })
 }
 
+
+  return (
+<>
+  <h1>Travel Blog</h1>
+    <form onSubmit={addLocation}>
+      Name: <input type='text' onChange={handleLocation}></input>
+      image: <input type='text' onChange={handleImg}></input>
+      Must See: <input type='text' onChange={handleSights}></input>
+      Top Restuants: <input type="number" onChange={handleRestaurants}></input>
+      Average Cost Per-Person: <input type='text' onChange={handleCost}></input>
+      Best Time of Year: <input type='text' onChange={handleTime}></input>
+      <input type="submit" value='Add Location'/>
+    </form>
+     <h1>Top Spots</h1> 
+     {/* {destination.map((spots) =>{
+      return(
+      <div key={spots._id}>
+        <h4>{spots.location}</h4>
+        <h4>{spots.sights}</h4>
+        <h4>{spots.img}</h4>
+        <h4>{spots.restaurants}</h4>
+        <h4>{spots.cost}</h4>
+        <h4>{spots.time}</h4>
+        <button onClick={(event) => handleLocationDelete(spots)}>Delete this Listing</button>
+      </div>
+       )}
+      )} */}
+
+
+     </>) 
+  
+}
 export default App;
+
+
+
+
+
+
+
+
