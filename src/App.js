a67a6e23b5472f945e37c868a0cb0f5d968514c6
Yulia Glushenko <yuliaglushenko@Yulias-MacBook-Air.local>
2022-06-04 11:42:16 -0400

    images for carousel folder

--- src/App.js
+++ src/App.js
@@ -3,16 +3,26 @@ import './App.css';
 import axios from 'axios'
 import 'bootstrap/dist/css/bootstrap.css'
 import { FaTimes, FaEdit, FaArrowUp, FaGlobeEurope, FaHandPointLeft, FaInstagramSquare, FaGithub, FaEnvelope } from "react-icons/fa"
-
+import iceland from './images/iceland.jpeg'
+import chile from './images/chile.jpeg'
+import indo from './images/indo.jpeg'
+import italy from './images/italy.jpeg'
+import japan from './images/japan.jpeg'
+import mexico from './images/mexico.jpeg'
+import slovenia from './images/slovenia_2.jpeg'
+import greece from './images/greece.jpeg'
+import south_korea from './images/south_korea.jpeg'
+import spain from './images/spain.jpeg'
+import thailand from './images/thailand.jpeg'
+import turkey from './images/turkey.jpeg'
+import south_africa from './images/south_africa.jpeg'
 import Carousel from 'react-bootstrap/Carousel'
 import { GoogleMap, LoadScript} from '@react-google-maps/api'
 import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container'
 import Nav from 'react-bootstrap/Nav'
 import NavDropdown from 'react-bootstrap/NavDropdown'
-import Button from 'react-bootstrap/Button'
-import FormControl from 'react-bootstrap/FormControl'
-import Form from 'react-bootstrap/Form'
+
 
 const mapContainerStyle = {width: '100%', height:300}
 
@@ -212,80 +222,80 @@ const handleChange = (e) => {
       
       <Carousel fade>
         <Carousel.Item interval={1500}>
-          <img className="d-block w-100" id ="car" src="https://res.cloudinary.com/itb-database/image/upload/s--IYQLolO6--/c_fill,dpr_auto,f_auto,q_auto:eco,w_1280/v1/Places/c29063d99be162886898118abbd053c1" alt="Image One"/>
+          <img className="d-block w-100" id ="car" src={iceland} alt="Image One"/>
           <Carousel.Caption>
             <h3>Iceland</h3>
         
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item interval={1500}>
-          <img className="d-block w-100" id ="car" src="https://www.capetown.travel/wp-content/uploads/2018/12/Helderberg_dusk_Craig_Howes.jpg" alt="Image Two"/>
+          <img className="d-block w-100" id ="car" src={south_africa} alt="Image Two"/>
           <Carousel.Caption>
             <h3>South Africa</h3>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item  interval={1500}>
-          <img className="d-block w-100 " id ="car" src="https://149359143.v2.pressablecdn.com/wp-content/uploads/2020/04/Canva-Trulli-of-Alberobello-Puglia-Italy-1024x684.jpg" alt="Image Three"/>
+          <img className="d-block w-100 " id ="car" src={italy} alt="Italy"/>
           <Carousel.Caption>
             <h3>Italy</h3>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item interval={1500}>
-          <img className="d-block w-100 " id ="car" src="https://media.architecturaldigest.com/photos/5c0ed0538d2a442e241057b1/16:9/w_2560%2Cc_limit/GettyImages-771579891.jpg" alt="Image Four"/>
+          <img className="d-block w-100 " id ="car" src={south_korea} alt="Korea"/>
           <Carousel.Caption>
             <h3>South Korea</h3>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item interval={1500}>
-          <img className="d-block w-100 " id ="car" src="https://static.onecms.io/wp-content/uploads/sites/9/2020/08/17/santiago-restaurant-bar-guide-FT-BLOG0820.jpg" alt="Image Five"/>
+          <img className="d-block w-100 " id ="car" src={chile} alt="Image Five"/>
           <Carousel.Caption>
             <h3>Chile</h3>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item interval={1500}>
-          <img className="d-block w-100 " id ="car" src="https://cdn.theculturetrip.com/wp-content/uploads/2017/06/24854155744_c61f21b3b9_k.jpg" alt="Turkey"/>
+          <img className="d-block w-100 " id ="car" src={turkey} alt="Turkey"/>
           <Carousel.Caption>
             <h3>Turkey</h3>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item interval={1500}>
-          <img className="d-block w-100 " id ="car" src="https://a.cdn-hotels.com/gdcs/production143/d1112/c4fedab1-4041-4db5-9245-97439472cf2c.jpg" alt="Indonesia"/>
+          <img className="d-block w-100 " id ="car" src={indo} alt="Indonesia"/>
           <Carousel.Caption>
             <h3>Indonesia</h3>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item interval={1500}>
-          <img className="d-block w-100 " id ="car" src="https://cdn.cnn.com/cnnnext/dam/assets/170606121226-japan---travel-destination---shutterstock-230107657.jpg" alt="Japan"/>
+          <img className="d-block w-100 " id ="car" src={japan} alt="Japan"/>
           <Carousel.Caption>
             <h3>Japan</h3>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item interval={1500}>
-          <img className="d-block w-100 " id ="car" src="https://www.fodors.com/wp-content/uploads/2021/05/UltimateMexicoCity__HERO_shutterstock_1058054480.jpg" alt="Mexico"/>
+          <img className="d-block w-100 " id ="car" src={mexico} alt="Mexico"/>
           <Carousel.Caption>
             <h3>Mexico</h3>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item interval={1500}>
-          <img className="d-block w-100 " id ="car" src="https://upload.wikimedia.org/wikipedia/commons/d/d3/Ljubljana_made_by_Janez_Kotar.jpg" alt="Slovenia"/>
+          <img className="d-block w-100 " id ="car" src={slovenia} alt="Slovenia"/>
           <Carousel.Caption>
             <h3>Slovenia</h3>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item interval={1500}>
-          <img className="d-block w-100 " id ="car" src='https://images.unsplash.com/photo-1506665531195-3566af2b4dfa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80' alt="Thailand"/>
+          <img className="d-block w-100 " id ="car" src={thailand} alt="Thailand"/>
           <Carousel.Caption>
             <h3>Thailand</h3>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item interval={1500}>
-          <img className="d-block w-100 " id ="car" src="https://images.unsplash.com/photo-1620677368158-32b1293fac36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="Spain"/>
+          <img className="d-block w-100 " id ="car" src={spain} alt="Spain"/>
           <Carousel.Caption>
             <h3>Spain</h3>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item interval={1500}>
-          <img className="d-block w-100 " id ="car" src="https://images.unsplash.com/photo-1469796466635-455ede028aca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="Greece"/>
+          <img className="d-block w-100 " id ="car" src={greece} alt="Greece"/>
           <Carousel.Caption>
             <h3>Greece</h3>
           </Carousel.Caption>
